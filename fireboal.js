aeabd65f82032f329093489debbd720e55f39686
tkc49 <h.tkc49@gmail.com>
2017-11-26 06:12:28 +0000

    Update fireboal.js

--- fireboal.js
+++ fireboal.js
@@ -3,11 +3,7 @@ const magik = magikcraft.io;
 function fireboal() {    
     
     magik.doNTimes(
-        function () { 
-            
-            magik.infierno();
-            
-        }, 1000, 300
+    magik.infierno(), 1000, 300
     );
     
     magik.infierno();
