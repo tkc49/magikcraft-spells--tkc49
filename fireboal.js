a711500f2ad787f36cbab1fef3341e4f2380011e
tkc49 <h.tkc49@gmail.com>
2017-11-26 06:28:26 +0000

    Update fireboal.js

--- fireboal.js
+++ fireboal.js
@@ -6,7 +6,7 @@ function fireboal() {
         function(){
             magik.dixit('tet');
             magik.infierno();
-        }, 1, 300
+        }, 1, 1000
     );
     
     //magik.infierno();
