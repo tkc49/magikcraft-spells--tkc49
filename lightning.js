64525ef0bafabc07bfef058102b5ad9ab70d3185
tkc49 <h.tkc49@gmail.com>
2017-12-10 06:20:00 +0000

    Update lightning.js

--- lightning.js
+++ lightning.js
@@ -3,7 +3,9 @@ const magik = magikcraft.io;
 function lightning() {
 
     var there = magik.aspecto();
-    magik.dixit(there);
+    magik.dixit(there.getX);
+    magik.dixit(there.getY);
+    magik.dixit(there.getZ);
 
     magik.doNTimes(
         function(){
