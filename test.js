7af7e1686fb6028d8a3b9482b21d20598439f0c1
tkc49 <h.tkc49@gmail.com>
2018-02-18 06:54:31 +0000

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,14 @@ const magik = magikcraft.io;
 
 function test() {
 
-    for( var i=1; i<=10; i++){
-        magik.dixit('aaa');
-    }    
+    for( var i = 1; i <= 10; i++ ){
+        magik.dixit('yamato');
+    }
+
+
+
+
+    // for( var i=1; i<=10; i++){
+    //     magik.dixit('aaa');
+    // }    
 }
