19c1a649b317e48c2dd67e6b48081aa17d2d1a73
tkc49 <h.tkc49@gmail.com>
2018-03-11 07:05:19 +0000

    Update comet.js

--- comet.js
+++ comet.js
@@ -1,8 +1,10 @@
 const magik = magikcraft.io;
 
 function comet(){
-    const Fireball = magik.type('entity.Fireball');
-    const TNT = magik.type("entity.EntityType").PRIMED_TNT;
+    // const Fireball = magik.type('entity.Fireball');
+    // const TNT = magik.type("entity.EntityType").PRIMED_TNT;
+
+
     const sender = magik.getSender();
     
     
@@ -26,29 +28,29 @@ function comet(){
 }
 
 
-function randn_bm() {
-    const u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
-    const v = 1 - Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-}
+// function randn_bm() {
+//     const u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+//     const v = 1 - Math.random();
+//     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+// }
 
 
-function rnorm_unit(count) {
-    let ret = 0;
-    for (let i = 0; i < count; i++) {
-        ret += Math.random();
-    }
-    return ret/count;
-}
-function rnorm(mean, sd) {
-    return randn_bm() * sd + mean;
-}
-function runif(from, to) {
-    return scale(Math.random(), from, to);
-}
-function scale(r, from, to) {
-    return Math.floor(r*(to - from)+from)
-}
-function rndBlock(radius, height) {
-    return magik.getSender().getLocation().getBlock().getRelative(rnorm(0, radius/5), rnorm(0, radius/5), rnorm(0, radius/5));
-}
+// function rnorm_unit(count) {
+//     let ret = 0;
+//     for (let i = 0; i < count; i++) {
+//         ret += Math.random();
+//     }
+//     return ret/count;
+// }
+// function rnorm(mean, sd) {
+//     return randn_bm() * sd + mean;
+// }
+// function runif(from, to) {
+//     return scale(Math.random(), from, to);
+// }
+// function scale(r, from, to) {
+//     return Math.floor(r*(to - from)+from)
+// }
+// function rndBlock(radius, height) {
+//     return magik.getSender().getLocation().getBlock().getRelative(rnorm(0, radius/5), rnorm(0, radius/5), rnorm(0, radius/5));
+// }
