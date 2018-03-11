44263c8d503d0480405c8f5f62fff39ebfbbfcad
tkc49 <h.tkc49@gmail.com>
2018-03-11 07:26:36 +0000

    Update comet.js

--- comet.js
+++ comet.js
@@ -15,7 +15,7 @@ function comet(){
         var arrow = player.getWorld().spawnEntity(sender.location, org.bukkit.entity.EntityType.ZOMBIE);
 
 
-        // arrow.setShooter(sender);
+        arrow.setShooter(sender);
         // var v = sender.getEyeLocation().getDirection().multiply(2);
         // v.setX(0);
         // v.setY(0);
