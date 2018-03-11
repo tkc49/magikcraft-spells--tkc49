39606ed55bf6f4a6a2e1bb57ffee9f027aa167a3
tkc49 <h.tkc49@gmail.com>
2018-03-11 05:32:46 +0000

    Update zombie.ts

--- zombie.ts
+++ zombie.ts
@@ -1,18 +1,15 @@
 const magik = magikcraft.io;
-
+ 
 function zombie(target: string){
     const EntityType = magik.type("entity.EntityType");
     const aZombie = EntityType['ZOMBIE'];
-    magik.dixit(EntityType);
-    
+     
     const where = magik.aspecto();
     const world = where.getWorld();
-
+ 
     const zombie = world.spawnEntity(where, aZombie); 
-
+ 
     if (target) {
         zombie.setTarget(magik.getPlugin().getServer().getPlayer(target));
-        magik.dixit("te");
     }
     magik.dixit("Behold!");
-}
