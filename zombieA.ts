b2a7b1883fd1f9714c95ac58c192996a02d6b1c7
tkc49 <h.tkc49@gmail.com>
2018-03-11 12:26:57 +0000

    Update zombieA.ts

--- zombieA.ts
+++ zombieA.ts
@@ -1,23 +1,13 @@
 const magik = magikcraft.io;
 
-function aaaa(){
-
+function zombieA(){
 
     const sender = magik.getSender();
     const EntityType = magik.type("entity.EntityType");
     const Zombie = EntityType['ZOMBIE'];
-
-    
-    magik.dixit('bbb');
     
     for (var i = 0; i < 50; i++) {
-
-
-        magik.dixit('aaa');
-        // var arrow = sender.getWorld().spawnEntity(sender.getEyeLocation(), Zombie);
         var arrow = sender.getWorld().spawnEntity(magik.aspecto(), Zombie);
-
-
     }
         
 }
