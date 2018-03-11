031ea1053b112526cfcc3af4787f9e18e8810f3c
tkc49 <h.tkc49@gmail.com>
2018-03-11 06:19:10 +0000

    Update z.ts

--- z.ts
+++ z.ts
@@ -3,5 +3,15 @@ var magik = magikcraft.io;
 function z(typ='ZOMBIE', n = 50, times = 5, baseloc){
     const EntityType = magik.type("entity.EntityType");
     const sender = magik.getSender();
+
+    if (typ == '.') {
+        typ = undefined;
+    }
+    if (!baseloc) {
+        baseloc = create_spawn_point(magik.aspecto());
+    }
+
+    
+ 
        
 }
