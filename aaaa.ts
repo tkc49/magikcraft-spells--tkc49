4c75be971d56784f80f453557b8cc339a9006b36
tkc49 <h.tkc49@gmail.com>
2018-03-11 12:20:04 +0000

    Update aaaa.ts

--- aaaa.ts
+++ aaaa.ts
@@ -4,13 +4,15 @@ function aaaa(){
 
 
     const sender = magik.getSender();
+
     
     magik.dixit('bbb');
     
     for (var i = 0; i < 50; i++) {
 
+
         magik.dixit('aaa');
-        var arrow = player.getWorld().spawnEntity(sender.location, org.bukkit.entity.EntityType.ZOMBIE);
+        var arrow = sender.getWorld().spawnEntity(sender.getEyeLocation(), org.bukkit.entity.EntityType.ZOMBIE);
 
 
     }
